dd37dff2610cc79c4555b992949a855758fb895d
muji <muji@tmpfs.org>
2022-08-27 17:06:51 +0800

    Sketch types for package index.

--- workspace/core/src/package.rs
+++ workspace/core/src/package.rs
@@ -1,9 +1,44 @@
-use serde::Deserialize;
+//! Types for package definitions.
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
 use semver::Version;
 
-/// Package descriptor.
-#[derive(Debug, Deserialize)]
+/// Address computed from a user's public key.
+type Address = [u8; 20];
+
+/// Hash of a package name to represent a package.
+type PackageId = [u8; 32];
+
+/// Describes a package.
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Descriptor {
     pub name: String,
     pub version: Version,
+}
+
+/// Definition of a package.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Definition {
+    /// The IPFS hash (cid) for the package blob.
+    pub hash: String,
+    /// Package descriptor.
+    pub descriptor: Descriptor,
+}
+
+/// Collection of package versions.
+#[derive(Default, Serialize, Deserialize)]
+pub struct Package {
+    versions: HashMap<Version, Definition>,
+}
+
+/// Packages for an account profile.
+#[derive(Default)]
+pub struct Profile {
+    packages: HashMap<PackageId, Package>,
+}
+
+/// Index of all available packages.
+#[derive(Default)]
+pub struct Index {
+    accounts: HashMap<Address, Profile>,
 }
