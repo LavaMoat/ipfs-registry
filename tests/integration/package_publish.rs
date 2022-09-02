81fc7bcfec8ea7a0b6ac22270ffa9a599b3e88cb
muji <muji@tmpfs.org>
2022-09-02 10:12:58 +0800

    Basic test spec for package publishing.

--- tests/integration/package_publish.rs
+++ tests/integration/package_publish.rs
@@ -1,7 +1,13 @@
+use std::path::PathBuf;
 use anyhow::Result;
 use serial_test::serial;
 
 use crate::test_utils::*;
+use semver::Version;
+
+use ipfs_registry_client::publish::publish_with_key;
+
+use k256::ecdsa::SigningKey;
 
 #[tokio::test]
 #[serial]
@@ -13,7 +19,19 @@ async fn integration_package_publish() -> Result<()> {
 
     let server_url = server();
 
-    println!("Run integration test {}", server_url);
+    let file = PathBuf::from("fixtures/mock-package-1.0.0.tgz");
+    let mime: mime::Mime = "application/gzip".parse()?;
+    let signing_key = SigningKey::random(&mut rand::thread_rng());
+
+    let receipt = publish_with_key(
+        server_url,
+        mime,
+        signing_key,
+        file,
+    ).await?;
+
+    assert_eq!("mock-package", receipt.artifact.package.name);
+    assert_eq!(Version::new(1, 0, 0), receipt.artifact.package.version);
 
     Ok(())
 }
